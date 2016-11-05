06ddcc2c9e3841367a5b78af2d7c9b8d50bd2dd0
Marcelo <marmosquera@gmail.com>
2016-11-05 11:43:17 -0300

    Commit Inicial

---
+++ ConversorXamarin/ConversorXamarin/Shared/Models/Conversor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConversorXamarin.Shared.Models
+{
+    public class Conversor 
+    {
+        public double kilometros { get; set; }
+        public double millas { get; set; }
+
+        public void Convertir()
+        {
+            millas = kilometros * 0.621371;
+        }
+    }
+}
---
+++ ConversorXamarin/ConversorXamarin/Shared/ViewModels/ConversorViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using Xamarin.Forms;
+using ConversorXamarin.Shared.Models;
+
+namespace ConversorXamarin.Shared.ViewModels
+{
+    public class ConversorViewModel : INotifyPropertyChanged
+    {
+        private Conversor conversor = new Conversor();
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string Kilometros
+        {
+            get { return conversor.kilometros.ToString(); }
+            set
+            {
+                if (conversor.kilometros.ToString() != value)
+                {
+                    double kms;
+                    if (Double.TryParse(value, out kms))
+                    {
+                        conversor.kilometros = kms;
+                        conversor.Convertir();
+                        if (PropertyChanged != null)
+                        {
+                            PropertyChanged(this, new PropertyChangedEventArgs("Millas"));
+                        }
+                    }
+                }
+            }
+        }
+
+        public string Millas
+        {
+            get { return conversor.millas.ToString(); }
+        }
+
+
+    }
+}
